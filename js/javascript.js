e0ee3b2017ac31b256b4ed5ee4fa87fefb02a4aa
fcroneberger <f.croneberger@deindesign.com>
2021-01-18 10:20:58 +0100

    Initial commit

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+const header = document.getElementById("header");
+const sticky = header.offsetTop;
+
+window.onscroll = function() {onScroll()};
+
+function onScroll() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+function toggleNav() {
+    header.classList.toggle("responsive");
+}
